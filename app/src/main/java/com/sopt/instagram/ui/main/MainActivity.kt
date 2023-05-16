b580f81f835cbe806be6b0781da55d5f2c5b591e
b1urrrr <b1urrr@naver.com>
2023-05-17 04:20:24 +0900

    [MOVE/#1] MainActivity ui 패키지로 이동

--- app/src/main/java/com/sopt/instagram/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/instagram/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sopt.instagram
+package com.sopt.instagram.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sopt.instagram.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
