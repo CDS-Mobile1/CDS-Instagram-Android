fe4f43e52a9da2d71997dbd753f7ed145b749b16
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-05-19 03:39:14 +0900

    [UI/#5] dm list fragment

---
+++ app/src/main/java/com/sopt/instagram/ui/main/DmActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.instagram.ui.main
+
+import android.os.Bundle
+import com.sopt.instagram.R
+import com.sopt.instagram.databinding.ActivityDmBinding
+import com.sopt.instagram.util.binding.BindingActivity
+
+class DmActivity : BindingActivity<ActivityDmBinding>(R.layout.activity_dm) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
