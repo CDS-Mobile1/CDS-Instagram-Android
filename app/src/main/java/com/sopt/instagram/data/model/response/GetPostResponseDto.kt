150458892206be73ebe18947465a777d1b15d3a5
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-19 02:40:33 +0900

    [ADD/#4] GetPostResponseDto 추가

---
+++ app/src/main/java/com/sopt/instagram/data/model/response/GetPostResponseDto.kt
@@ -0,0 +1,24 @@
+package com.sopt.instagram.data.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetPostResponseDto(
+    @SerialName("content")
+    val content: String,
+    @SerialName("imageUrlList")
+    val imageUrlList: List<String>,
+    @SerialName("userInfo")
+    val userInfo: UserInfo
+) {
+    @Serializable
+    data class UserInfo(
+        @SerialName("memberId")
+        val memberId: Int,
+        @SerialName("memberImageUrl")
+        val memberImageUrl: String,
+        @SerialName("memberName")
+        val memberName: String
+    )
+}
