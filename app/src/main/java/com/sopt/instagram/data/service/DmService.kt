b37967e039f66fc2085d91f79c962c7e5cc298b7
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-05-19 21:40:37 +0900

    [FIX#5] DmSerive 수정

--- app/src/main/java/com/sopt/instagram/data/service/DmService.kt
+++ app/src/main/java/com/sopt/instagram/data/service/DmService.kt
@@ -1,4 +1,5 @@
 package com.sopt.instagram.data.service
+import com.sopt.instagram.data.model.response.DmResponseDTO
 import com.sopt.instagram.data.model.response.wrapper.BaseResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -6,6 +7,6 @@ import retrofit2.http.GET
 interface DmService {
     @GET("~/api/friend/favorites/stories")
     suspend fun getStories(
-        @Body request: DmService,
-    ): BaseResponse<DmService>
+        @Body request: DmResponseDTO,
+    ): BaseResponse<DmResponseDTO>
 }
