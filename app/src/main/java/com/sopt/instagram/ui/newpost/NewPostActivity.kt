12cc5eb95b48e1e054c9ac287b37d8bfcb2b9f7a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-20 23:03:31 +0900

    [ADD/#4] newpost 추가

---
+++ app/src/main/java/com/sopt/instagram/ui/newpost/NewPostActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.instagram.ui.newpost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sopt.instagram.R
+
+class NewPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_post)
+    }
+}
