225ba1ca84949a4d6f88215ecf8929960477d33a
b1urrrr <b1urrr@naver.com>
2023-05-20 18:42:00 +0900

    [FEAT/#3] viewpager 연결 및 coil 적용

--- app/src/main/java/com/sopt/instagram/ui/story/StoryActivity.kt
+++ app/src/main/java/com/sopt/instagram/ui/story/StoryActivity.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import com.sopt.instagram.R
 import com.sopt.instagram.databinding.ActivityStoryBinding
 import com.sopt.instagram.util.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StoryActivity : BindingActivity<ActivityStoryBinding>(R.layout.activity_story) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_story)
+
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        with(binding) {
+            vpStory.adapter = StoryAdapter(this@StoryActivity)
+            vpStory.isUserInputEnabled = false
+        }
     }
 }
--- app/src/main/java/com/sopt/instagram/ui/story/StoryAdapter.kt
+++ app/src/main/java/com/sopt/instagram/ui/story/StoryAdapter.kt
@@ -3,23 +3,13 @@ package com.sopt.instagram.ui.story
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.sopt.instagram.domain.entity.Member
 
-class StoryAdapter(
-    fragmentActivity: FragmentActivity,
-    private val members: List<Member>,
-) : FragmentStateAdapter(fragmentActivity) {
-    private val fragmentList = createStoryFragment()
+class StoryAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    private val fragmentList = listOf<Fragment>(
+        StoryFragment(),
+    )
 
-    private fun createStoryFragment(): List<StoryFragment> {
-        val fragmentList: MutableList<StoryFragment> = mutableListOf()
-        for (i in 1..members.size) {
-            fragmentList.add(StoryFragment())
-        }
-        return fragmentList
-    }
-
-    override fun getItemCount() = members.size
+    override fun getItemCount(): Int = fragmentList.size
 
     override fun createFragment(position: Int): Fragment = fragmentList[position]
 }
