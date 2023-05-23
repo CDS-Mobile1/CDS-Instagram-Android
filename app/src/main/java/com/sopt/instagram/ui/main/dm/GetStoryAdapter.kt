d60682ce5b92998a94521093294fff7ae6a574b0
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-05-23 15:12:20 +0900

    [FIX/#5] ktlint 반영

--- app/src/main/java/com/sopt/instagram/ui/main/dm/GetStoryAdapter.kt
+++ app/src/main/java/com/sopt/instagram/ui/main/dm/GetStoryAdapter.kt
@@ -7,38 +7,37 @@ import com.bumptech.glide.Glide
 import com.sopt.instagram.data.model.response.DmstoryResponseDto
 import com.sopt.instagram.databinding.ItemDmStarredBinding
 
-class GetStoryAdapter : ListAdapter<DmstoryResponseDto,GetStoryAdapter.GetStoryViewHolder>(
-    GetStoryCallback){
-    class GetStoryViewHolder(private val binding: ItemDmStarredBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(item: DmstoryResponseDto){
-            with(binding){
-                tvDmStarredName.text=item.memberName
+class GetStoryAdapter : ListAdapter<DmstoryResponseDto, GetStoryAdapter.GetStoryViewHolder>(
+    GetStoryCallback,
+) {
+    class GetStoryViewHolder(private val binding: ItemDmStarredBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: DmstoryResponseDto) {
+            with(binding) {
+                tvDmStarredName.text = item.memberName
                 Glide.with(root)
                     .load(item.memberImageUrl)
                     .into(ibDmStarredProfile)
-
             }
         }
     }
 
-    companion object{
-        private val GetStoryCallback=object :DiffUtil.ItemCallback<DmstoryResponseDto>(){
+    companion object {
+        private val GetStoryCallback = object : DiffUtil.ItemCallback<DmstoryResponseDto>() {
             override fun areItemsTheSame(oldItem: DmstoryResponseDto, newItem: DmstoryResponseDto): Boolean {
-                return oldItem.hashCode()==newItem.hashCode()
+                return oldItem.hashCode() == newItem.hashCode()
             }
 
             override fun areContentsTheSame(
                 oldItem: DmstoryResponseDto,
-                newItem: DmstoryResponseDto
+                newItem: DmstoryResponseDto,
             ): Boolean {
-                return oldItem==newItem
+                return oldItem == newItem
             }
-
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GetStoryViewHolder {
-        val binding=ItemDmStarredBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = ItemDmStarredBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return GetStoryViewHolder(binding)
     }
 
