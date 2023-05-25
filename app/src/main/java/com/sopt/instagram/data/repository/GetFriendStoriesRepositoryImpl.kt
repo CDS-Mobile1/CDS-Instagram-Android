177ecf13e78a28d3150e117ee5b222b4b81f3be6
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-25 23:45:12 +0900

    [MOD/#12] 스코프 함수 it to response

--- app/src/main/java/com/sopt/instagram/data/repository/GetFriendStoriesRepositoryImpl.kt
+++ app/src/main/java/com/sopt/instagram/data/repository/GetFriendStoriesRepositoryImpl.kt
@@ -1,17 +1,17 @@
 package com.sopt.instagram.data.repository
 
 import com.sopt.instagram.data.source.GetFriendStoriesSource
-import com.sopt.instagram.domain.entity.GetFriendStoriesEntity
+import com.sopt.instagram.domain.entity.FriendStories
 import com.sopt.instagram.domain.repository.GetFriendStoriesRepository
 import javax.inject.Inject
 
 class GetFriendStoriesRepositoryImpl @Inject constructor(
     private val getFriendStoriesSource: GetFriendStoriesSource,
 ) : GetFriendStoriesRepository {
-    override suspend fun getFriendStories(): Result<List<GetFriendStoriesEntity>?> =
+    override suspend fun getFriendStories(): Result<List<FriendStories>?> =
         runCatching {
-            getFriendStoriesSource.getFriendStoriesList().data?.map {
-                it.toGetFriendStoriesEntity()
+            getFriendStoriesSource.getFriendStoriesList().data?.map { response ->
+                response.toGetFriendStoriesEntity()
             }
         }
 }
--- app/src/main/java/com/sopt/instagram/data/repository/GetPostRepositoryImpl.kt
+++ app/src/main/java/com/sopt/instagram/data/repository/GetPostRepositoryImpl.kt
@@ -1,17 +1,17 @@
 package com.sopt.instagram.data.repository
 
 import com.sopt.instagram.data.source.GetPostDataSource
-import com.sopt.instagram.domain.entity.GetPostEntity
+import com.sopt.instagram.domain.entity.Post
 import com.sopt.instagram.domain.repository.GetPostRepository
 import javax.inject.Inject
 
 class GetPostRepositoryImpl @Inject constructor(
     private val getPostDataSource: GetPostDataSource,
 ) : GetPostRepository {
-    override suspend fun getPostList(): Result<List<GetPostEntity>?> =
+    override suspend fun getPostList(): Result<List<Post>?> =
         runCatching {
-            getPostDataSource.getPostList().data?.map {
-                it.toGetPostEntity()
+            getPostDataSource.getPostList().data?.map { response ->
+                response.toGetPostEntity()
             }
         }
 }
