4e4d4dbb9979269ff09533f3556b69490c00eae0
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-26 07:06:12 +0900

    [MOD/#4] Multi ViewType 구현

--- app/src/main/java/com/sopt/instagram/ui/home/PostAdapter.kt
+++ app/src/main/java/com/sopt/instagram/ui/home/PostAdapter.kt
@@ -5,14 +5,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.sopt.instagram.R
+import com.sopt.instagram.databinding.ItemHomeRecommendFriendBinding
 import com.sopt.instagram.databinding.ItemPostBinding
 import com.sopt.instagram.domain.entity.Post
 import com.sopt.instagram.util.DiffCallback
 
-class PostAdapter : ListAdapter<Post, PostAdapter.PostViewHolder>(diffUtil) {
+class PostAdapter : ListAdapter<Post, RecyclerView.ViewHolder>(diffUtil) {
     class PostViewHolder(private val binding: ItemPostBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun onBind(post: Post) {
+        fun onBindPost(post: Post) {
             with(binding) {
                 data = post
                 ivHomeUserProfileImage.load(post.memberImageUrl)
@@ -24,18 +26,60 @@ class PostAdapter : ListAdapter<Post, PostAdapter.PostViewHolder>(diffUtil) {
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        return PostViewHolder(
-            ItemPostBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false,
-            ),
-        )
+    class RecommendFriendViewHolder(private val binding: ItemHomeRecommendFriendBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBindRecommendFriend() {
+            binding.layoutRecommendFriendList.adapter = PostRecommendFriendAdapter().apply {
+                submitList(
+                    listOf(
+                        R.drawable.img_recommend_friend,
+                        R.drawable.img_recommend_friend,
+                        R.drawable.img_recommend_friend,
+                        R.drawable.img_recommend_friend,
+                    ),
+                )
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        holder.onBind(getItem(position))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_POST -> PostViewHolder(
+                ItemPostBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false,
+                ),
+            )
+
+            VIEW_TYPE_RECOMMEND_FRIEND -> RecommendFriendViewHolder(
+                ItemHomeRecommendFriendBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false,
+                ),
+            )
+
+            else -> throw ClassCastException(
+                parent.context.getString(
+                    R.string.view_type_error_msg,
+                    viewType,
+                ),
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is PostAdapter.PostViewHolder) {
+            holder.onBindPost(getItem(position))
+        } else if (holder is RecommendFriendViewHolder) holder.onBindRecommendFriend()
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            1 -> VIEW_TYPE_RECOMMEND_FRIEND
+            else -> VIEW_TYPE_POST
+        }
     }
 
     companion object {
@@ -43,5 +87,10 @@ class PostAdapter : ListAdapter<Post, PostAdapter.PostViewHolder>(diffUtil) {
             onItemsTheSame = { old, new -> old.memberId == new.memberId },
             onContentsTheSame = { old, new -> old == new },
         )
+
+        private const val RECOMMEND_FRIEND_COUNT = 1
+
+        const val VIEW_TYPE_RECOMMEND_FRIEND = 0
+        const val VIEW_TYPE_POST = 1
     }
 }
