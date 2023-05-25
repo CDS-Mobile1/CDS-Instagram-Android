8c86ef9534d0c5e366e7c19f2d255d7d19ec270a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-25 16:48:33 +0900

    [MOD/#12] UiState 사용

--- app/src/main/java/com/sopt/instagram/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/instagram/ui/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.sopt.instagram.R
 import com.sopt.instagram.databinding.FragmentHomeBinding
+import com.sopt.instagram.util.UiState
 import com.sopt.instagram.util.binding.BindingFragment
+import com.sopt.instagram.util.extension.showToast
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val viewModel by viewModels<HomeViewModel>()
 
@@ -21,8 +25,23 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
     }
 
     private fun initAdapter() {
-        binding.rvHomePost.adapter = PostAdapter().apply {
-            submitList(viewModel.mockPostList)
+        viewModel.getPostList()
+        viewModel.getPostListState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    binding.rvHomePost.adapter = PostAdapter().apply {
+                        submitList(viewModel.postList.value)
+                    }
+                }
+
+                is UiState.Failure -> {
+                    requireContext().showToast("erererer")
+                }
+
+                else -> {
+                    // nothing
+                }
+            }
         }
     }
 }
