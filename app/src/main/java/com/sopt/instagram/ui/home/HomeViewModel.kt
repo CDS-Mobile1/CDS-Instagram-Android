73ac2057ee80a595a20eebca4a59f6ff11d9231a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-25 18:18:41 +0900

    [FEAT/#12] getFriendStoriesList 구현

--- app/src/main/java/com/sopt/instagram/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/instagram/ui/home/HomeViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sopt.instagram.domain.entity.GetFriendStoriesEntity
 import com.sopt.instagram.domain.entity.GetPostEntity
+import com.sopt.instagram.domain.repository.GetFriendStoriesRepository
 import com.sopt.instagram.domain.repository.GetPostRepository
 import com.sopt.instagram.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,6 +17,7 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val getPostRepository: GetPostRepository,
+    private val getFriendStoriesRepository: GetFriendStoriesRepository,
 ) : ViewModel() {
     private val _postList = MutableLiveData<List<GetPostEntity>>()
     val postList: LiveData<List<GetPostEntity>>
@@ -22,6 +25,12 @@ class HomeViewModel @Inject constructor(
 
     private val _getPostListState = MutableLiveData<UiState>()
     val getPostListState: LiveData<UiState> get() = _getPostListState
+
+    private val _friendStoriesList = MutableLiveData<List<GetFriendStoriesEntity>>()
+    val friendStoriesList: LiveData<List<GetFriendStoriesEntity>> get() = _friendStoriesList
+
+    private val _getFriendStoriesListState = MutableLiveData<UiState>()
+    val getFriendStoriesListState: LiveData<UiState> get() = _getFriendStoriesListState
 
     // 서버 통신
     fun getPostList() {
@@ -35,6 +44,20 @@ class HomeViewModel @Inject constructor(
                 .onFailure {
                     _getPostListState.value = UiState.Failure(null)
                     Timber.tag("getPostFailure").d(it)
+                }
+        }
+    }
+
+    fun getFriendStoriesList() {
+        viewModelScope.launch {
+            getFriendStoriesRepository.getFriendStories()
+                .onSuccess { response ->
+                    _friendStoriesList.value = response
+                    _getFriendStoriesListState.value = UiState.Success
+                }
+                .onFailure {
+                    _getFriendStoriesListState.value = UiState.Failure(null)
+                    Timber.tag("getFriendStoriesList").d(it)
                 }
         }
     }
