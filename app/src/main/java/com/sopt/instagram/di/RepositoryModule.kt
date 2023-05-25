9de561cfbfd4296b4508191f20eb8f3895d8e519
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-25 16:49:40 +0900

    [MOD/#12] di getPost Module 구현

--- app/src/main/java/com/sopt/instagram/di/RepositoryModule.kt
+++ app/src/main/java/com/sopt/instagram/di/RepositoryModule.kt
@@ -1,9 +1,19 @@
 package com.sopt.instagram.di
 
+import com.sopt.instagram.data.repository.GetPostRepositoryImpl
+import com.sopt.instagram.domain.repository.GetPostRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindsGetPostRepository(
+        authRepositoryImpl: GetPostRepositoryImpl,
+    ): GetPostRepository
+}
--- app/src/main/java/com/sopt/instagram/di/ServiceModule.kt
+++ app/src/main/java/com/sopt/instagram/di/ServiceModule.kt
@@ -1,9 +1,18 @@
 package com.sopt.instagram.di
 
+import com.sopt.instagram.data.service.GetPostService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object ServiceModule
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun getPostService(retrofit: Retrofit): GetPostService =
+        retrofit.create(GetPostService::class.java)
+}
